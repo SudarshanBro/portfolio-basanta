a3c703b0d3bdd559cd0c0948254bbe2408238a18
Sudarshan_0126 <128712927+SudarshanBro@users.noreply.github.com>
2025-08-25 14:24:19 +0545

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,15 +7,15 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <h1>Test - App is working!</h1>
-      {/* Temporarily comment out other components to test basic functionality */}
-      {/*
+      
+      {
       <Navigation />
       <Hero />
       <Education />
       <Hobbies />
       <Gallery />
       <RecentActivity />
-      */}
+      }
     </div>
   );
 };
