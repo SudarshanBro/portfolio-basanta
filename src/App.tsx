d2cd31cb66ee7dd0c21da0209bd2e5fb4667c3cb
Sudarshan_0126 <128712927+SudarshanBro@users.noreply.github.com>
2025-08-25 14:17:45 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { Gallery } from '@/components/Gallery';
-import { Education } from '@/components/Education';
-import { Hero } from '@/components/Hero';
-import { RecentActivity } from '@/components/RecentActivity';
-import { Hobbies } from '@/components/Hobbies';
 
 const queryClient = new QueryClient();
 
@@ -18,20 +13,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          { <Navigation />
-      <Hero />
-      <Education />
-      <Hobbies />
-      <Gallery />
-      <RecentActivity />
-      
-          }
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
